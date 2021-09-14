cafc5e61c94c7c14c54e3812fb7950b5d05bea92
dabideee13 <d.e.magno13@gmail.com>
2021-09-14 14:04:00 +0800

    Update: add open terminal function

--- utils.zsh
+++ utils.zsh
@@ -1,6 +1,17 @@
 #!/usr/local/bin/zsh
 
 
+# terminal
+function term() {
+    if $# -eq 0
+    then
+        open -a iterm -n "$PWD"
+    else
+        open -a iterm -n "$1"
+    fi
+}
+
+
 # jupyter
 function nb() {
     jupyter nbconvert $1 --to html
