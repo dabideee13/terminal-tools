4387d76b571db5f09e6caebc95aeff073a18f9ba
dabideee13 <d.e.magno13@gmail.com>
2021-09-21 12:17:07 +0800

    Refactor: separate functions

--- utils.zsh
+++ utils.zsh
@@ -29,33 +29,4 @@ function dl_playlist() {
 }
 
 
-# search 
-function find_helper() {
-    ARRAY=()
-    for i in $(find ./ $1 $2)
-    do
-        ARRAY+=$PWD${i#"./"}"\n"
-    done
-    echo $ARRAY
-}
 
-
-function find_extension() {
-    echo "$(find_helper -iname $1)"
-}
-
-
-function find_pattern() {
-    STRING='*'$1'*'
-    find_extension $STRING
-}
-
-
-function search() {
-    if { echo $1 | grep -q '*' }
-        then
-            find_extension $1
-    else 
-        find_pattern $1
-    fi
-}
