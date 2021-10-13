36dd65695f3f36a42c4c700ae6afbe7e123a261b
dabideee13 <d.e.magno13@gmail.com>
2021-10-13 12:25:52 +0800

    Update

--- utils.py
+++ utils.py
@@ -1,6 +1,10 @@
+#!/Library/Frameworks/Python.framework/Versions/3.9/bin/python3
+
 """
 Utility functions
 """
+
+from pathlib import Path
 
 
 def list_packages() -> list:
@@ -8,3 +12,17 @@ def list_packages() -> list:
         packages = f.read().split('\n')
     return packages
 
+
+def list_dir(path: Optional[Union[str, Path]] = None) -> List[Path]:
+    if path:
+        files = Path(path).iterdir()
+
+    pass
+
+
+def get_project_name() -> str:
+    while
+
+
+def go_source() -> Path:
+    pass
--- utils.zsh
+++ utils.zsh
@@ -1,13 +1,23 @@
 #!/usr/local/bin/zsh
 
 
+function th() {
+    cd $TH
+}
+
+
+function ignore() {
+    wget "https://raw.githubusercontent.com/dabideee13/terminal-tools/main/.gitignore"
+}
+
+
 # terminal
 function term() {
     if $# -eq 0
     then
-        open -a iterm -n "$PWD"
+        open -a iterm -n "$PWD" 2>/dev/null
     else
-        open -a iterm -n "$1"
+        open -a iterm -n "$1" 2>/dev/null
     fi
 }
 
@@ -29,8 +39,40 @@ function dl_playlist() {
 }
 
 
+# search 
+function find_helper() {
+    ARRAY=()
+    for i in $(find ./ $1 $2)
+    do
+        ARRAY+=$PWD${i#"./"}"\n"
+    done
+    echo $ARRAY
+}
+
+
+function find_extension() {
+    echo "$(find_helper -iname $1)"
+}
+
+
+function find_pattern() {
+    STRING='*'$1'*'
+    find_extension $STRING
+}
+
+
+function search() {
+    if { echo $1 | grep -q '*' }
+        then
+            find_extension $1
+    else 
+        find_pattern $1
+    fi
+}
+
+
 function activate() {
-    source venv/bin/activate 2>/dev/null || 
-    source ../venv/bin/activate 2>/dev/null || 
+    source venv/bin/activate 2>/dev/null ||
+    source ../venv/bin/activate 2>/dev/null ||
     source ../../venv/bin/activate
 }
