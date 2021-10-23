b7f80cb8db473b9e45445fc8c7e739510d50cce7
Dave Emmanuel Magno <56486609+dabideee13@users.noreply.github.com>
2021-10-23 08:29:00 +0800

    Update explore.py

--- explore.py
+++ explore.py
@@ -3,24 +3,47 @@
 import argparse
 import os
 import subprocess
-from typing import Optional
 
 
-def explore(file_extension: Optional[str] = None) -> None:
-    playground_path = "/Users/d.e.magno/Temporary/playground"
-    extension = file_extension.strip().lower()
+class Explorer:
 
-    file_type = [ext for ext in os.listdir(playground_path) if extension == ext][0]
-    final_path = playground_path + f"/{file_type}"
+    def __init__(self, file_extension: str) -> None:
+        self._file_extension = file_extension.strip().lower()
+        self._playground_path = '/Users/d.e.magno/Temporary/playground'
+        self._new_dir = None
+        self._final_path = None
 
-    command = f"code {final_path}"
-    subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+    def _check_dir(self):
+        if self._file_extension in os.listdir(self._playground_path):
+            return True
+        return False
+
+    def explore(self) -> None:
+        self._final_path = f"{self._playground_path}/{self._file_extension}"
+
+        if not self._check_dir():
+            os.mkdir(self._final_path)
+            self._run(self._file_command)
+
+        self._run(self._code_command)
+
+    @property
+    def _file_command(self) -> str:
+        return f"touch {self._final_path}/playground.{self._file_extension}"
+
+    @property
+    def _code_command(self) -> str:
+        return f"code {self._final_path}"
+
+    def _run(self, command: str) -> None:
+        subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument("TYPE", type=str)
-
     args = parser.parse_args()
 
-    explore(args.TYPE)
+    explore = Explorer(args.TYPE)
+    explore.explore()
